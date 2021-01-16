14d5cffd0fb1debff0743c37dd11bb2cf4db70ed
Andreas <andreas.lang@student.upt.ro>
2021-01-16 23:37:12 +0200

    BUrciu

---
+++ me_myself/src/Nav Bar/nav-bar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const navBar = () =>
+{
+    return (
+        <div>
+            <div>Home</div>
+        </div>
+    );
+}
+
+export default navBar;
---
+++ me_myself/src/Nav Bar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const navBar = (props) =>
+{
+    render()
+    {
+        return (
+            <div>
+                <div>Home</div>
+            </div>
+        );
+    }
+}
+
+export default navBar;
